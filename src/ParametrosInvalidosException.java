19d3025b1feff61a144a7bbda7cfb0e06297b293
Kaio Brenner <kaiobrenner460@protonmail.com>
2023-10-19 20:01:38 -0300

    first commit

---
+++ src/ParametrosInvalidosException.java
@@ -0,0 +1,9 @@
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException() {
+        super("Parâmetros inválidos.");
+    }
+
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
